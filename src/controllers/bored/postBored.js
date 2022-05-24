5760e17d9c3e4ebf0ee44968de4029b00f480d5e
Binh Nguyen <binh.nguyen@credera.co.uk>
2022-05-25 00:37:49 +0100

    feat: add a button to the /bored endpoint

--- src/controllers/bored/postBored.js
+++ src/controllers/bored/postBored.js
@@ -10,7 +10,9 @@ export default async function postBored(req, res) {
   try {
     const boredApiUrl = BORED_API_PATH;
 
-    const { activity } = await got(boredApiUrl, { method: 'GET' }).json();
+    const { key: activityKey, activity } = await got(boredApiUrl, {
+      method: 'GET',
+    }).json();
 
     const suggestive = suggestives[randIntBetween(0, suggestives.length - 1)];
 
@@ -23,6 +25,23 @@ export default async function postBored(req, res) {
             type: 'mrkdwn',
             text: `_${activity}, ${suggestive}._`,
           },
+        },
+        {
+          type: 'actions',
+          elements: [
+            {
+              type: 'button',
+              text: {
+                type: 'plain_text',
+                text: 'Give me something else?',
+                emoji: true,
+              },
+              // The activity's ID (key) is passed to the button's value so we
+              // can identify which "activity" triggered the refresh button.
+              value: activityKey,
+              action_id: 'yodabored-button-refresh-activity',
+            },
+          ],
         },
       ],
     });
